5c5c78e61e1bac6498ebdfbdd96cff378e38cefe
Krishnan <pkrishnan.007aylr@gmail.com>
2025-02-16 18:37:34 -0500

    Create next_greater_element.py

---
+++ next_greater_element.py
@@ -0,0 +1,33 @@
+# For each element in nums1, find the index j such that nums1[i] == nums2[j] and 
+# determine the next greater element of nums2[j] in nums2. 
+# If there is no next greater element, then the answer for this query is -1.
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        arr = []
+        ind = {}
+        for i,v in enumerate(nums2):
+            ind[v] = i
+        for i,v in enumerate(nums1):
+            idx = ind[v]
+            if idx < len(nums2)-1:
+                for j in range(idx+1,len(nums2)):
+                    if nums2[j] > v:
+                        arr.append(nums2[j])
+                        break
+                    elif j==len(nums2)-1:
+                        arr.append(-1)
+            else:
+                arr.append(-1)
+        return arr
+      
+def main():
+    obj = Solution()
+    print(obj.nextGreaterElement([2,4],[1,2,3,4]))
+  
+if __name__ == '__main__':
+    main()
