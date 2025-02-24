b87f2d4fbdab96ea308b9c8faa6804cea38ffaf3
Krishnan <pkrishnan.007aylr@gmail.com>
2025-02-24 14:56:54 -0500

    Solution using monotonic stack

--- next_greater_element.py
+++ next_greater_element.py
@@ -4,11 +4,7 @@
 class Solution(object):
     def nextGreaterElementBruteForce(self, nums1, nums2):
         """
-        Brute force solution
-        Time complexity: O(n*m) 
-        Space complexity: O(n+m)
-        n: size of nums1
-        m: size of nums2
+        Brute force solution using hashmap
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
@@ -29,10 +25,32 @@ class Solution(object):
             else:
                 arr.append(-1)
         return arr
+
+    def nextGreaterElementUsingStack(self, nums1, nums2):
+            """
+            Solution using monotonic stack and hashmap
+            :type nums1: List[int]
+            :type nums2: List[int]
+            :rtype: List[int]
+            """
+            stk = []
+            ind = {}
+            n = len(nums1)
+            res = [-1]*n
+            for i,v1 in enumerate(nums1):
+                ind[v1] = i
+            for j,v2 in enumerate(nums2):
+                while stk and v2 > stk[-1]:
+                    val = stk.pop()
+                    if val in nums1:
+                        res[ind[val]] = v2
+                stk.append(v2)
+            return res
       
 def main():
     obj = Solution()
     print(obj.nextGreaterElementBruteForce([2,4],[1,2,3,4]))
+    print(obj.nextGreaterElementUsingStack([2,4],[1,2,3,4]))
   
 if __name__ == '__main__':
     main()
