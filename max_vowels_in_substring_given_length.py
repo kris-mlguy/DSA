8bb3907e9bdd6329e854c2ebba0d7c523095bb4d
Krishnan <pkrishnan.007aylr@gmail.com>
2025-06-04 13:29:40 +0530

    Sliding window method

--- max_vowels_in_substring_given_length.py
+++ max_vowels_in_substring_given_length.py
@@ -1,6 +1,6 @@
 # Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
 class Solution(object):
-    def maxVowels(self, s, k):
+    def maxVowelsBruteForce(self, s, k):
         """
         Brute force method
         :type s: str
@@ -18,4 +18,21 @@ class Solution(object):
                     curr_sum +=1
             if curr_sum > max_sum:
                 max_sum = curr_sum     
+        return max_sum
+     def maxVowelsSlidingWindow(self, s, k):
+        """
+        Sliding window method
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        n = len(s)
+        vowels = {'a','e','i','o','u'}
+        p1, curr_sum, max_sum = 0, 0, 0
+        for p2 in range(n):
+            curr_sum += 1 if s[p2] in vowels else 0
+            if p2-p1+1>k:
+                curr_sum -= 1 if s[p1] in vowels else 0
+                p1+=1
+            max_sum = max(max_sum,curr_sum)
         return max_sum
