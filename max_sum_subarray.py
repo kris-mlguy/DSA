06d123f02cc5821f9120717e72104f999a205df7
Krishnan <pkrishnan.007aylr@gmail.com>
2025-07-06 16:15:57 +0530

    Add sliding window method

--- max_sum_subarray.py
+++ max_sum_subarray.py
@@ -13,3 +13,17 @@ class Solution(object):
                 curr_sum += nums[j]
                 max_sum = max(max_sum, curr_sum)
         return max_sum
+    def maxSubArraySlidingWindow(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        curr_sum = max_sum = nums[0]
+        for i in range(1,n):
+            if curr_sum<0 and curr_sum<nums[i]:
+                curr_sum = nums[i]
+            else:
+                curr_sum+=nums[i]
+            max_sum = max(max_sum,curr_sum)
+        return max_sum
