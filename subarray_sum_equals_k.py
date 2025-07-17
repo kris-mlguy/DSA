1f486a4054f5854dd9a599fb9deb7de8989587e4
Krishnan <pkrishnan.007aylr@gmail.com>
2025-07-17 13:41:46 +0530

    Update subarray_sum_equals_k.py

--- subarray_sum_equals_k.py
+++ subarray_sum_equals_k.py
@@ -1,5 +1,20 @@
 # Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
 class Solution(object):
+    def subarraySumBruteForce(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(nums)
+        res = 0
+        for i in range(n):
+            curr_sum = 0
+            for j in range(i,n):
+                curr_sum += nums[j]
+                if curr_sum==k:
+                    res+=1
+        return res
     def subarraySumHashmapSlidingWindow(self, nums, k):
         """
         :type nums: List[int]
